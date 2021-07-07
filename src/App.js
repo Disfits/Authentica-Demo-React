c32639319235b82ec2f9e91d286b75d8ca785df2
Arpit Bhardwaj <arpitbhardwaj@Arpits-MacBook-Pro.local>
2021-07-08 01:16:47 +0530

    Removed Contract Address

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import getWeb3 from "./web3";
 import { useEffect, useState } from "react";
 import abi from "./ABI.json";
 import ipfs from "./ipfs";
+import JSONObject from "./IPFS_interface.json";
+import Github from "./github.png";
 
 function App() {
   const [account, setAccount] = useState();
   const [loadState, setLoadState] = useState("loading");
   const [web3, setWeb3] = useState();
   const [contract, setContract] = useState();
+  const [signedIn, setSignedIn] = useState(false);
   const organisationId = 42069;
 
   const connectWallet = () => {
@@ -31,7 +34,7 @@ function App() {
     if (web3 && account) {
       const newContract = new web3.eth.Contract(
         abi,
-        "0x17B5e878415403f86b9EB490416711670C95f97d",
+        "0xFa24bAf1A4c513b3b55A128aB63c4868af12fAe2",
         {
           from: account.toString(),
         }
@@ -44,33 +47,34 @@ function App() {
     connectWallet();
   }, []);
 
-  function storeJSON(params){
+  function storeJSON(params) {
     //Pass metadata JSON
-    (async () => {
-      const metaData = JSON.stringify(JSONObject);
-      const cid = await ipfs.add(metaData);
-      console.log("IPFS cid:", cid);
-    
-  })()
-}
+    const metaData = JSON.stringify(JSONObject);
+    ipfs.add(metaData).then((res) => {
+      console.log("IPFS cid:", res);
+    });
+  }
 
   function handleSignIn(params) {
     // Sign In code goes here
     // Pass Org Id and Token Id
     contract.methods.signIn(organisationId).call((err, result) => {
       console.log(result);
+      setSignedIn(true);
     });
     // Handle error and result
   }
   function handleSignUp(params) {
-    //Sign Up code goes here
-    //Pass account URI and organisation ID
+    // Sign Up code goes here
+    // Pass account URI and organisation ID
     contract.methods
       .signUp("test arpit", organisationId)
       .send((err, result) => {
         console.log(result);
         console.log(err);
       });
+
+    storeJSON();
     //Handle error and result
   }
   return (
@@ -78,30 +82,65 @@ function App() {
       <header className="App-header">
         <div className="card">
           <h3>NFAuth Demo</h3>
-          <button className="secondary" onClick={handleSignIn}>
-            Sign in using NFAuth
-          </button>
-          <button className="primary" onClick={handleSignUp}>
-            Sign up using NFAuth
-          </button>
-          <div className="load-state">
-            {loadState === "loading" ? (
-              <div>Connecting wallet...</div>
-            ) : loadState === "error" ? (
-              <div>
-                Wallet Not connected.{" "}
-                <span
-                  onClick={() => {
-                    connectWallet();
-                  }}
-                  className="connect-text"
-                >
-                  Try again
-                </span>
+          {signedIn ? (
+            <div>You're signed in</div>
+          ) : (
+            <div>
+              <button className="secondary" onClick={handleSignIn}>
+                Sign in using NFAuth
+              </button>
+              <button className="primary" onClick={handleSignUp}>
+                Sign up using NFAuth
+              </button>
+              <div className="load-state">
+                {loadState === "loading" ? (
+                  <div>Connecting wallet...</div>
+                ) : loadState === "error" ? (
+                  <div>
+                    Wallet Not connected.{" "}
+                    <span
+                      onClick={() => {
+                        connectWallet();
+                      }}
+                      className="connect-text"
+                    >
+                      Try again
+                    </span>
+                  </div>
+                ) : (
+                  <div>Wallet connected</div>
+                )}
               </div>
-            ) : (
-              <div>Wallet connected</div>
-            )}
+            </div>
+          )}
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: 30,
+            whiteSpace: "nowrap",
+            display: "flex",
+          }}
+        >
+          <div style={{ paddingTop: "4px" }}>
+            <img
+              src={Github}
+              style={{ display: "inline", height: "20px", width: "20px" }}
+            ></img>
+          </div>
+          <div>
+            <a
+              href="https://github.com/arpitbhardwaj24"
+              style={{
+                display: "inline",
+                fontSize: "18px",
+                marginLeft: "5px",
+                color: "#bbb",
+                textDecoration: "none",
+              }}
+            >
+              arpitbhardwaj24
+            </a>
           </div>
         </div>
       </header>
--- src/ipfs.js
+++ src/ipfs.js
@@ -1,4 +1,10 @@
-const IPFS = require('ipfs-http-client')
-const ipfs = new IPFS({host:'ipfs.infura.io',port:5001,protocol:'https'})
+// const IPFS = require("ipfs-api");
+// const ipfs = new IPFS({
+//   host: "ipfs.infura.io",
+//   port: 5001,
+//   protocol: "https",
+// });
+
+const ipfs = require("nano-ipfs-store").at("https://ipfs.infura.io:5001");
 
 export default ipfs;
